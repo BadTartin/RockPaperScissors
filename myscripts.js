f59b46fcd97cf2f63e180aa6c9e6141c60613a6c
Tad Martin <tadfmartin@gmail.com>
2022-07-28 09:24:15 -0400

    added styles file, header, and buttons working on results div

--- myscripts.js
+++ myscripts.js
@@ -3,35 +3,45 @@
 // generate random choice from computer
 function getComputerChoice () {
     const choices = ["rock", "paper", "scissors"];
-    ind = Math.floor(Math.random()*3);
-    return choices[ind];
+    indx = Math.floor(Math.random()*3);
+    return choices[indx];
 }
-
 
 // lookup table for results
 const outcomes = {
     rock:    { rock: ['Tie!', 0, 0], paper: ['Paper Beats Rock! You Lose!', 0, 1], scissors: ['Rock Beats Scissors! You Win!', 1, 0] },
     paper:   { rock: ['Paper Beats Rock! You Win!', 1, 0], paper: ['Tie!', 0, 0], scissors: ['Scissors Beat Paper! You Lose!', 0, 1] },
-    scissors: { rock: ['Rock beats scissors! You Lose!', 0, 1], paper: ['Scissors Beats Paper! You Win!', 1, 0], scissors: ['Tie!', 0.0] }
+    scissors: { rock: ['Rock beats Scissors! You Lose!', 0, 1], paper: ['Scissors Beats Paper! You Win!', 1, 0], scissors: ['Tie!', 0.0] }
   };
 
 //   play one round
 function playRound (playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
-    return outcomes[playerSelection][computerSelection]
+    console.log(outcomes[playerSelection][computerSelection]);
 }
 
 //  play five rounds of the game and calculate a final score
-function game() {
-    var playerScore = 0,
-    computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        roundResult = playRound(prompt("Rock, Paper, or Scissors?"), getComputerChoice())
-        playerScore += roundResult[1]
-        computerScore += roundResult[2]
-        console.log(roundResult[0])
-    }
-    console.log('Final Score- Player:', playerScore, 'Computer:', computerScore)
-}
+// function game() {
+//     var playerScore = 0,
+//     computerScore = 0;
+//     for (let i = 0; i < 5; i++) {
+//         roundResult = playRound(prompt("Rock, Paper, or Scissors?"), getComputerChoice())
+//         playerScore += roundResult[1]
+//         computerScore += roundResult[2]
+//         console.log(roundResult[0])
+//     }
+//     console.log('Final Score- Player:', playerScore, 'Computer:', computerScore)
+// }
 
-game()
+// game()
+
+
+const btn = document.querySelectorAll(".btn");
+
+btn.forEach(btn => btn.addEventListener("click", 
+  () => playRound(btn.innerText, getComputerChoice())));
+
+const rslt = document.querySelector(".results");
+
+rslt.innerText = 'Final Score- Player:', playerScore, 'Computer:', computerScore
+
